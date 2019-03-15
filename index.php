ef1ce1d57894c2293bb5bed1446e3abc52c23c75
Dinesh Mittal <32518898+dmittal11@users.noreply.github.com>
2019-03-15 10:30:32 +0000

    Changes Made

--- index.php
+++ index.php
@@ -1,8 +1,32 @@
 <?php
 
+date_default_timezone_set('Europe/London');
+        $date = date('d/m/Y h:i:s a', time());
+		
+		var_dump($date);
+		
+/*
+
 include_once "simple_html_dom.php";
 
-$dom = file_get_html('http://www.dexel.co.uk/themes/bootstrap/js/bootstrap.js');
+$file = "js.html";
+
+$current = "<script>\n";
+
+//file_put_contents($file, $current);
+
+//$current .= file_get_contents('http://www.dexel.co.uk/themes/bootstrap/js/bootstrap.js');
+
+$current .= file_get_contents('test.js');
+
+$current .= "\n </script>";
+
+file_put_contents($file, $current);
+
+
+
+
+$dom = file_get_html('http://localhost/test/js.html');
 
 //$dom = str_get_html('http://www.dexel.co.uk/themes/bootstrap/js/bootstrap.js');
 
@@ -30,11 +54,14 @@ while($scripts = $dom->find('script', 0)->innertext){
 }
 */
 
-$scripts = $dom->find('script', 0)->innertext;
+//$scripts = $dom->find('script', 0)->innertext;
 
-$matches = array();
-	$matches = preg_split('/(?:)(.*?)(?:")/', $scripts);
-	print_r($matches);
+//$matches = array();
+/*
+	$matches = preg_match_single('/var h = (.+);/', $dom);
+	var_dump($matches);
+	
+	#/var allTyres = (.+);/
 	
 	/*
 
@@ -72,7 +99,12 @@ foreach($scripts as $s){
 //print_r($dom);
 
 
-
+/*
+* preg_match() and return match
+*/
+function preg_match_single($pattern, $subject) {
+   return preg_match($pattern, $subject, $matches) ? end($matches) : null;
+}
 
 
 
---
+++ test.js
@@ -0,0 +1,24 @@
+  var h = [{
+  "id": "210",
+  "phone_id": "234",
+  "computer_id": "235"
+  }];
+  
+
+  
+  var hello = "Hello";
+
+	hello();
+	
+	    var i = [{
+  "id": "210",
+  "phone_id": "234",
+  "computer_id": "235"
+  }];
+
+  
+  function hello() {
+	  
+	  console.log('Hello');
+	  
+  }
